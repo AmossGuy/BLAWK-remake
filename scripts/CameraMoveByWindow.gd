2d2e3a4d7b79962b8670dd79d254f09c28275092
AmossGuy <yellowilya@gmail.com>
2020-07-03 14:58:39 -0400

    add initial physics objects

--- scripts/CameraMoveByWindow.gd
+++ scripts/CameraMoveByWindow.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
+func _ready():
+	get_node("/root/WindowScaler").set_window_position(get_position())
+
 func _process(delta):
 	set_position(get_node("/root/WindowScaler").get_window_position())
