429d06576b5399f04c8a2bab1fb8f35947a5d16a
AmossGuy <yellowilya@gmail.com>
2020-07-10 15:47:03 -0400

    fix warning

--- scripts/CameraMoveByWindow.gd
+++ scripts/CameraMoveByWindow.gd
@@ -3,5 +3,5 @@ extends Node2D
 func _ready():
 	get_node("/root/WindowScaler").set_window_position(get_position())
 
-func _process(delta):
+func _process(_delta):
 	set_position(get_node("/root/WindowScaler").get_window_position())
